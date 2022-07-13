7eb3758c26187c7ece2605efcf1d29816aebecbb
Adekunle Fatima <logun.fateem@gmail.com>
2022-07-13 20:03:30 +0100

    OOP

--- class30-materials/class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,20 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class EspressoMachine {
+  constructor(model, make, color, price) {
+   this.model = model
+   this.make = make
+   this.color = color
+   this.price = price
+  }
+
+  turnOn() {
+   console.log('Good day, I am now on!!')
+  }
+   brew(){
+    console.log("brew!!!!!")
+   }
+
+   steam() {
+    console.log ("ITS SUMMERRRRR SALES 50%%%%%%")
+   }
+  }
