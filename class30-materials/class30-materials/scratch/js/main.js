4e09c8c16ceb9175b8cfbb93d8f2fd3a3388937f
Adekunle Fatima <logun.fateem@gmail.com>
2022-07-14 23:56:37 +0100

    New

--- class30-materials/class30-materials/scratch/js/main.js
+++ class30-materials/class30-materials/scratch/js/main.js
@@ -29,13 +29,84 @@
 // let salem = new Cat('Salem', 'American')
 
 // THE IDEA
-/* If the main character attacks the target, the target's health will reduce by the damage done*/
+/* If the main character attacks the target, the target's health will reduce by the damage done
+If at the end of the game, the main character's health is greater than zero it means that the character eliminated the target*/
 
-class Character {
- constructor(name, health, strength) {
-   this.name = name;
-   this.health = health;
-   this.strength = strength;
-   this.xp = 0; // XP is always zero for new characters
+// class Character {
+//  constructor(name, health, strength) {
+//    this.name = name;
+//    this.health = health;
+//    this.strength = strength;
+//    this.xp = 0; // XP is always zero for new characters
+//  }
+
+//  attack(target) {
+//   if (this.health > 0) {
+//    let damage = this.strength
+//    // console.log(`${this.name} eliminated ${target.name} by ${damage} damage point`)
+//    console.log(`${this.name} attacks ${target.name} and causes ${damage} damage points`)
+//   target.health -= damage;
+//   if (target.health > 0) {
+//    // console.log(`${this.name} eliminated ${target.name} by ${damage} damage point`)
+//    console.log(`${target.name} has ${target.health} health points `)
+//   }
+//   else {
+//    target.health = 0
+//    let bonusXP = 10
+//    console.log(`${this.name} eliminated ${target.name} and earned ${bonusXP} bonus points`)
+//    this.xp += bonusXP
+//   }
+  
+//  } 
+//  else {
+//   console.log(`${this.name} can't attack (they've been eliminated)`);
+//  }
+// }
+
+//   describe() {
+//    console.log (`${this.name} has ${this.health} health points, ${this
+//     .strength} as strength and ${this.xp} XP points`) ;
+//   }
+// }
+
+// const aurora = new Character("Aurora", 150, 25);
+// const glacius = new Character("Glacius", 130, 30);
+
+// console.log("Welcome to the adventure! Here are our heroes:");
+// console.log(aurora.describe());
+// console.log(glacius.describe());
+
+// const monster = new Character("Spike", 40, 20);
+// console.log("A wild monster has appeared: it's named " + monster.name);
+
+// monster.attack(aurora);
+// monster.attack(glacius);
+// aurora.attack(monster);
+// glacius.attack(monster);
+
+// console.log(aurora.describe());
+// console.log(glacius.describe());
+
+class Dog {
+ constructor(name, species, size) {
+  this.name = name 
+  this.species = species
+  this.size = size
+ }
+
+ bark() {
+  if (this.size > 40) {
+   this.bark() = 'GRRRR GRRRR'
+  } 
+  else {
+   this.bark() = "Woof! Woof!"
+  }
+ }
+ describe() {
+  console.log(`${this.name} is a ${this.species} dog measuring ${this.size}`);
+  console.log(`Look, a cat! ${this.name} barks: ${this.bark()}`);
  }
 }
+
+const fang = new Dog("Fang", "boarhound", 75);
+console.log(fang.describe())
