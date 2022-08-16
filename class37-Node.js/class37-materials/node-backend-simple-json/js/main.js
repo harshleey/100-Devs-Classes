5044356e7e050aed039142e225a3f223e994f5b9
Wonuola <logun.fateem@gmail.com>
2022-08-16 01:09:49 +0100

    New commit

--- class37-Node.js/class37-materials/node-backend-simple-json/js/main.js
+++ class37-Node.js/class37-materials/node-backend-simple-json/js/main.js
@@ -8,6 +8,6 @@ async function makeReq(){
 
   console.log(data);
   document.querySelector("#personName").textContent = data.name
-  document.querySelector("#personStatus").textContent = data.status
-  document.querySelector("#personOccupation").textContent = data.currentOccupation
+  // document.querySelector("#personStatus").textContent = data.status
+  // document.querySelector("#personOccupation").textContent = data.currentOccupation
 }
